fab3402caed37c45de29d780a6e2447e57d494e4
Stanislav Kravchenko <stankra@yandex.ru>
2020-04-02 15:45:58 +0300

    Add pg username

--- config/test.exs
+++ config/test.exs
@@ -9,13 +9,6 @@ config :cookpod, Cookpod.Repo,
   port: 6432,
   pool: Ecto.Adapters.SQL.Sandbox
 
-# Configure the database for GitHub Actions
-if System.get_env("GITHUB_ACTIONS") do
-  config :app, App.Repo,
-    username: "postgres",
-    password: "postgres"
-end
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :cookpod, CookpodWeb.Endpoint,
