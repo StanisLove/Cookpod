3a08d98aea44056d18e5885a818ba7cb0bdace4c
Stanislav Kravchenko <stankra@yandex.ru>
2020-04-07 17:07:47 +0300

    Add slime

--- config/dev.exs
+++ config/dev.exs
@@ -62,7 +62,7 @@ config :cookpod, CookpodWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/cookpod_web/(live|views)/.*(ex)$",
-      ~r"lib/cookpod_web/templates/.*(eex)$"
+      ~r"lib/cookpod_web/templates/.*(eex|slim|slime)$"
     ]
   ]
 
