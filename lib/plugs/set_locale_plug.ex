5cba2a7bb64201fc5a47c8be477f4fcc7b2c8950
Stanislav Kravchenko <stankra@yandex.ru>
2020-04-08 08:12:56 +0300

    Save locale to cookie

--- lib/plugs/set_locale_plug.ex
+++ lib/plugs/set_locale_plug.ex
@@ -3,15 +3,28 @@ defmodule CookpodWeb.Plugs.SetLocale do
   Specifying the desired locale is via the URL.
   """
 
+  import Plug.Conn
+
   @supported_locales Gettext.known_locales(CookpodWeb.Gettext)
+  @locale_max_age 365 * 24 * 60 * 60
 
   def init(_options), do: nil
 
-  def call(%Plug.Conn{params: %{"locale" => locale}} = conn, _options)
-  when locale in @supported_locales do
-    CookpodWeb.Gettext |> Gettext.put_locale(locale)
-    conn
+  def call(conn, _options) do
+    case fetch_locale_from(conn) do
+      nil ->
+        conn
+
+      locale ->
+        CookpodWeb.Gettext |> Gettext.put_locale(locale)
+        conn |> put_resp_cookie("locale", locale, max_age: @locale_max_age)
+    end
   end
 
-  def call(conn, _options), do: conn
+  defp fetch_locale_from(conn) do
+    (conn.params["locale"] || conn.cookies["locale"]) |> check_locale
+  end
+
+  defp check_locale(locale) when locale in @supported_locales, do: locale
+  defp check_locale(_), do: nil
 end
