70889f54b9119efecf245c2ed4733620ac28ff26
Stanislav Kravchenko <stankra@yandex.ru>
2020-04-18 18:03:48 +0300

    Add model test

--- lib/cookpod/user.ex
+++ lib/cookpod/user.ex
@@ -13,6 +13,7 @@ defmodule Cookpod.User do
     field :surname, :string
     field :password_hash, :string
     field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
@@ -39,7 +40,7 @@ defmodule Cookpod.User do
   def new_changeset, do: changeset(%User{}, %{})
 
   defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
-    change(changeset, password_hash: add_hash(password))
+    change(changeset, password_hash: Bcrypt.hash_pwd_salt(password))
   end
 
   defp put_pass_hash(changeset), do: changeset
